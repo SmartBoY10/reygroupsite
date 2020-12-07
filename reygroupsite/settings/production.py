19cbc60b1d1502051a77e69c4d24479672152134
Qurol Abdujalilov <qurol.abduajalilov99@gmail.com>
2020-12-08 00:24:02 +0500

    Change allowed hosts to

--- reygroupsite/settings/production.py
+++ reygroupsite/settings/production.py
@@ -34,7 +34,7 @@ SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['reygroup.herokuapp.com']
+ALLOWED_HOSTS = ['reygroup.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
