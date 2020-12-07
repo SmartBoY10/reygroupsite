f1dd167dab72ed4f4f061d7054c183a8481af667
Qurol Abdujalilov <qurol.abduajalilov99@gmail.com>
2020-12-08 02:16:51 +0500

    ROOTs

--- reygroupsite/settings/local_settings.py
+++ reygroupsite/settings/local_settings.py
@@ -139,12 +139,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'reygroupsite/static'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 MEDIA_URL = '/media/'
 
 INTERNAL_IPS = ['127.0.0.1']
